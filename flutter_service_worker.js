cffe73dfb63bc6cd52e08cab84c4920ed34e085e
wchklaus <41989304+wchklaus@users.noreply.github.com>
2022-09-09 13:39:16 +0000

    Build: (936be2e) update config

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -18,14 +18,10 @@ const RESOURCES = {
 "assets/favicon.png": "e990c85d612f13b66faa12c37ac913d3",
 "assets/manifest.json": "0fbf286ee180c096b0a05669cf323bcf",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
-"index.html": "2670c5652b3b30e3bdfc300aa7518d05",
-"/": "2670c5652b3b30e3bdfc300aa7518d05",
+"index.html": "dbf8e69819015b8ed2cadd7027d38dd5",
+"/": "dbf8e69819015b8ed2cadd7027d38dd5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"version.json": "a74ce0bee35cc93dee71211e4d8908f1",
-"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1"
+"version.json": "a74ce0bee35cc93dee71211e4d8908f1"
 };
 
 // The application shell files that are downloaded before a service worker can
