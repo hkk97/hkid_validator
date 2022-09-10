5dabb36de2b667dcaa1427902219b76e858af83d
wchklaus <41989304+wchklaus@users.noreply.github.com>
2022-09-10 08:37:45 +0000

    Build: (acc69ea) update ui

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
 "canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
 "canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62",
-"main.dart.js": "98e32985ccf907dbf5571916fd8131bb",
+"main.dart.js": "7819de09b6211cf853bcfc44a667ed40",
 "assets/AssetManifest.json": "d9266bbd42b3547b3bd3ad9e714ddb78",
 "assets/assets/FontManifest.json": "f66aa55cd79651f341aedd8491362eea",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
@@ -22,8 +22,8 @@ const RESOURCES = {
 "assets/favicon.png": "e990c85d612f13b66faa12c37ac913d3",
 "assets/manifest.json": "0fbf286ee180c096b0a05669cf323bcf",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
-"index.html": "a18d071a3374dc8021a91d735149a02a",
-"/": "a18d071a3374dc8021a91d735149a02a",
+"index.html": "4638ef1cc257f38358b3406a8deb594f",
+"/": "4638ef1cc257f38358b3406a8deb594f",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "a74ce0bee35cc93dee71211e4d8908f1"
 };
