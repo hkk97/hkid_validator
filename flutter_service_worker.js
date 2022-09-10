2f2437459f091a0c3021439410e926c3e71dfba9
wchklaus <41989304+wchklaus@users.noreply.github.com>
2022-09-10 14:56:20 +0000

    Build: (d1c5e10) update loading gif and theme

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
 "canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
 "canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62",
-"main.dart.js": "69a071b7246406278f6cd3a0c3f36b14",
+"main.dart.js": "a8e8e56bd942c00015939fc110a79079",
 "assets/AssetManifest.json": "161ac874ad73b08ba4443d3f8aecb309",
 "assets/assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
 "assets/assets/FontManifest.json": "f66aa55cd79651f341aedd8491362eea",
@@ -27,8 +27,8 @@ const RESOURCES = {
 "assets/loading.gif": "6def8d80a999aa526b1eeb5799c215c7",
 "assets/manifest.json": "0fbf286ee180c096b0a05669cf323bcf",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
-"index.html": "b1026543f53573358c3d063c6a42e4ff",
-"/": "b1026543f53573358c3d063c6a42e4ff",
+"index.html": "0314c4624395c4f5feeb399fd8f2afa4",
+"/": "0314c4624395c4f5feeb399fd8f2afa4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "a74ce0bee35cc93dee71211e4d8908f1"
 };
