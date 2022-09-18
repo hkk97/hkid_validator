f4a3e48b7decfed1ce49a6a58371a1f5c1cfe1a5
wchklaus <41989304+wchklaus@users.noreply.github.com>
2022-09-18 10:34:21 +0000

    Build: (92cbc4b) change back to apt-get install

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -59,8 +59,8 @@ const RESOURCES = {
 "assets/apple-touch-icon-60x60-precomposed.png": "bc5ad2f49eb3dad6392425125dcdd555",
 "assets/apple-touch-icon-144x144-precomposed.png": "3199c713fbd8bd3dc3c339a50ccb4629",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
-"index.html": "f812a580052b39ed906ef964af53e38d",
-"/": "f812a580052b39ed906ef964af53e38d",
+"index.html": "960cca6df09a07c09fc0c1e807b690cf",
+"/": "960cca6df09a07c09fc0c1e807b690cf",
 "version.json": "a74ce0bee35cc93dee71211e4d8908f1"
 };
 
